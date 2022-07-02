44a6c7e135934664d1a3df3636d6ed4bdba70e81
Neto Daniel Zinga <danielnzinga5@gmail.com>
2022-07-03 01:34:19 +0200

    definition de la function lamp() qu'allume l'ampoule si elle n'est pas cassée

--- lampe.js
+++ lampe.js
@@ -4,3 +4,9 @@ const lamp = document.getElementById('lamp');
 function isLampBroken () {
     return lamp.src.indexOf('quebrada') > -1;
 }
+
+function lampOn () {
+    if (!isLampBroken()) {
+        lamp.src = './img/ligada.jpg';
+    }
+}
